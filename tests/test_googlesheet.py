e6a1438d29b85a8c679066cc9b0c86f96b7e5af9
senseibara <ndiayebara93@gmail.com>
2022-09-01 12:10:41 +0000

    add update worksheet feature to google sheets module

--- tests/test_googlesheet.py
+++ tests/test_googlesheet.py
@@ -1,5 +1,6 @@
+import pandas as pd
 from dotenv import load_dotenv
-from propersandwich.googlesheet import read_worksheet, create_worksheet, delete_worksheet
+from propersandwich.googlesheet import read_worksheet, create_worksheet, delete_worksheet, update_worksheet
 
 load_dotenv()
 
@@ -12,4 +13,13 @@ def test_create_worksheet():
     create_worksheet('test_sheet',worksheet_tab_name)
     data = read_worksheet('test_sheet',worksheet_tab_name)
     assert len(data.index) == 0
+    delete_worksheet('test_sheet',worksheet_tab_name)
+
+def test_update_worksheet():
+    worksheet_tab_name = 'random test worksheet'
+    list_of_numbers = [10, 20, 30, 40, 50, 60]
+    data = pd.DataFrame(list_of_numbers, columns=['Numbers'])
+    update_worksheet('test_sheet', worksheet_tab_name, data)
+    data_from_updated_worksheet = read_worksheet('test_sheet', worksheet_tab_name)
+    assert len(data_from_updated_worksheet.index) == 6
     delete_worksheet('test_sheet',worksheet_tab_name)
